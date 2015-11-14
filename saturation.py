67ce452830d58fa34f91d3653c9aba44a5f7699e
Caiit <Caitlin.Lagrand@live.nl>
2015-11-14 18:13:27 +0100

    goal detection

--- saturation.py
+++ saturation.py
@@ -10,11 +10,11 @@ while(vid.isOpened()):
 
 	hsv = cv2.cvtColor(f,cv2.COLOR_BGR2HSV)
 	s = hsv[:,1:]
-	im = cv2.GaussianBlur(s, (1,1), 0, 0)
+	im = cv2.GaussianBlur(s, (13,13), 0, 0)
 
 	edges = cv2.Canny(im,60,180,apertureSize = 3,  L2gradient=True)
 
-	lines = cv2.HoughLinesP(edges,10,np.pi/2,100, minLineLength = 70, maxLineGap = 10)
+	lines = cv2.HoughLinesP(edges,10,np.pi/2,100, minLineLength = 40, maxLineGap = 10)
 	if lines != None: 
 		for x1,y1,x2,y2 in lines[0]:
 			cv2.line(f,(x1,y1),(x2,y2),(0,255,0),2)
