528931125e39228c9de4dff1a0a3c5e3bcb22f42
Michiel van der Meer <michielmeer@live.nl>
2015-12-02 13:52:11 +0100

    added y offset, needs testing

--- live_contours.py
+++ live_contours.py
@@ -3,7 +3,7 @@ import numpy as np
 from naoqi import ALProxy
 import vision_definitions
 
-IP = "192.168.1.66"
+IP = "192.168.0.101"
 PORT = 9559
 
 # Create proxy to nao
@@ -20,8 +20,13 @@ width = 320
 height = 240
 image = np.zeros((height, width, 3), np.uint8)
 
-# For a distance of 0.4 meter, the radius of the tomato is 26
+# for a distance of 0.4 meter, the radius of the tomato is 26
 radiusToMeters = 26*0.4
+# the width of the real tomato is 0.05 meter, on a distance of 0.40 meter it is 46 pixels
+pixToMeters = 46*0.4
+pixToMeters = 0.05/46*0.4
+# the tomato lies always on a height of 0.35 meters
+z = 0.35
 
 nameId = camProxy.subscribe("python_GVM", resolution, colorSpace, fps)
 print 'getting images in remote'
@@ -46,7 +51,7 @@ while True:
 
 		# Prepare image
 		blur = cv2.medianBlur(image,5)
-		red = cv2.inRange(blur,np.array((0,140, 70)),np.array((10,250,200)))
+		red = cv2.inRange(blur,np.array((0,140, 60)),np.array((10,250,200)))
 		erode = cv2.erode(red,None,iterations = 3)
 		dilate = cv2.dilate(erode,None,iterations = 10)
 
@@ -62,8 +67,15 @@ while True:
 				cv2.circle(image,center,radius,(0,255,0),2)
 				cv2.circle(image,center,2,(0,0,255),2)
 
-			distance = (radiusToMeters / radius)
-			print(distance)
+			x_distance = (radiusToMeters / radius)
+			if x < 160:
+				y = pixToMeters*(160-x)
+			else:
+				y = pixToMeters*(x-160)
+
+			x_distance = (radiusToMeters / radius)
+			print "X_distance:",x_distance
+			print "Y_offset:",y
 
 		cv2.imshow('findContours', image)
 		if cv2.waitKey(120) & 0xFF == ord('q'):
